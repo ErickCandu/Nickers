bc46f4c0aaf7b2d92cf7ce9f8978892a4af60d86
Erick Candú <erickcandu@outlook.com>
2024-12-17 17:58:10 -0300

    Add files via upload

---
+++ Projeto-Nickers/script.js
@@ -0,0 +1,13 @@
+function redirectTo(type) {
+    if (type === 'work') {
+        window.location.href = 'work.html';
+    } else if (type === 'gaming') {
+        window.location.href = 'gaming.html';
+    } else if (type === 'valor') {
+        window.location.href = 'valor.html';
+    } else if (type === 'notebook') {
+        window.location.href = 'notebook.html';
+    } else if (type === 'voltar') {
+        window.location.href = 'index.prim.html';
+    }
+}
